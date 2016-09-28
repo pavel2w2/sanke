6cd05066e9be0d0fff6ed57538c21c60c2a60721
pasha <pavel@mail.ru>
2016-09-28 23:54:02 +0300

    инкапсуляция  и создание 2х конструкторов

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,10 +10,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
+            Point p1 = new Point(1,3,'*');
+            /*p1.x = 1;
             p1.y = 3;
-            p1.sym = '*' ;
+            p1.sym = '*' ;*/
             p1.Drow();
 
             Point p2 = new Point();
